5bb90fcf99d81715720ca4f51088b19281d6a9c0
Dionmar Lariosa <76565431+bem-bem@users.noreply.github.com>
2022-05-11 16:12:00 +0800

    remove resource admin

--- resources/views/admin/categories/_form.blade.php
+++
@@ -1,9 +0,0 @@
-
-<div class="mb-3">
-  <x-inputs.label for="name">{{ __('Category name') }}</x-inputs.label>
-  <x-inputs.input name="name" value="{{ old('name' , optional($category ?? null)->name) }}" autofocus="{{ true }}" />
-</div>
-<div class="mb-3">
-  <x-inputs.label for="slug">{{ __('Category Slug') }}</x-inputs.label>
-  <x-inputs.input name="slug" value="{{ old('slug' , optional($category ?? null)->slug) }}" />
-</div>
